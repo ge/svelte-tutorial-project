7a2d9ff8c27297d599518b9512dd97f656643094
George Entenman <zabouti@gmail.com>
2019-09-06 16:13:02 -0400

    Ready to use ContactCard Component
    Many changes to show how local button and input tags work.

--- src/App.svelte
+++ src/App.svelte
@@ -1,4 +1,6 @@
 <script>
+	import CC from "./ContactCard.svelte";
+
 	let name = "ge";
 	let age = 30;
 
@@ -15,7 +17,8 @@
 		age += 1;
 	}
 
-	function defaultName() {
+	/* Not really deprecated.  This will work if needed. */
+	function deprecatedDefaultName() {
 		name = "zabouti";
 	}
 
@@ -32,5 +35,16 @@
 </style>
 
 <h1>Hello {name}!</h1>
-<button type="text"  on:click="{defaultName}">DefaultName</button>
+
+<h2>Button action</h2>
+<p>This example (and its associated function) have been deprecated because the value is hardwired.</p>
+<button type="text"  on:click="{deprecatedDefaultName}">DefaultName (Deprecated)</button>
+
+<h2>Form input</h2>
+<p>This first input field works just fine, but it's more verbose internally than the following example.</p>
 <input type="text" value="{name}" on:input="{replaceName}">
+<p>This input field uses svelte keyword <em>bind</em>, which replaces the function needed in the first input field.  This is very clever.</p>
+<input type="text" bind:value="{name}">
+
+
+<CC />
