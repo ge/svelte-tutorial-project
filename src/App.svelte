7c4194bfe053bdb96c71d9725acccd588c6d08fc
George Entenman <zabouti@gmail.com>
2019-09-06 13:37:23 -0400

    Lesson 2.13
    Can now enter a new name for the app to display.
    
    Learned  howboth *button* and *input* tags work with svelte

--- src/App.svelte
+++ src/App.svelte
@@ -1,5 +1,28 @@
 <script>
-	export let name;
+	let name = "ge";
+	let age = 30;
+
+	$: upperCaseName=name.toUpperCase();
+	$: console.log(name);
+	$: if (name === "ge") {
+		console.log("found expected name");
+		age = 76;
+	} else {
+		console.log("did not find expected name");
+	}
+
+	function incrementAge() {
+		age += 1;
+	}
+
+	function defaultName() {
+		name = "zabouti";
+	}
+
+	function replaceName(event) {
+		name = event.target.value;
+	}
+
 </script>
 
 <style>
@@ -9,3 +32,5 @@
 </style>
 
 <h1>Hello {name}!</h1>
+<button type="text"  on:click="{defaultName}">DefaultName</button>
+<input type="text" value="{name}" on:input="{replaceName}">
